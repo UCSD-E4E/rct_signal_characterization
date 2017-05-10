b7bde731cb26fd2e8a60c76a2c234c650cbcf85f
Thomas An <thomasan1995@gmail.com>
2017-05-10 15:58:03 -0700

    Fixed status check to perform at the start. Status check keeps running until GPS fix found or error thrown

--- UI-Script/ui-start.sh
+++ UI-Script/ui-start.sh
@@ -1,88 +1,116 @@
 # -* bash *-
 #!/bin/sh
 
-timestamp() {
-	date
-}
-
 PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin
 
 #Sourcing file to get correct logs and directories
 INSTALL_DIR=&INSTALL_PREFIX
 source $INSTALL_DIR/etc/rct_config
 
-case "$1" in
-    #STATUS will check for correct file and output path, then start checking for GPS lock
-    status)
-        #Checking for valid output directory
-        OUTPUT_CHECK=0
-        if [ ! -e $output_dir ]
-            then
-            echo "$output_dir not found"
-            exit 1
+# Checking for valid output directory
+sudo python ./gpsFixCheck.py -i /dev/ttyACM0 -b 115200 >> /var/log/gpsFix.log
+
+OUTPUT_CHECK=1
+FILE_CHECK=1
+GPS_LOCK=1
+SDR_CHECK=1
+DISK_SPACE=1
+
+OVERALL_STATUS=1
+
+timestamp() {
+    date
+}
+
+status () {
+    while [ "$OVERALL_STATUS" -eq "1" ]
+    do
+        if [ -w $output_dir ]; then
+            echo "$output_dir writeable"
         else 
             OUTPUT_CHECK=1
-            echo "$output_dir valid"
-        fi
-
-        #Check if file exists
-        FILE_CHECK=0
-        if [ -e "$log_dir/rct_gps_log.log" ] then
-            FILE="$log_dir/rct_gps_log.log"
-            FILE_CHECK=1
-        else
-            echo "$log_dir/rct_gps_log.log not a valid file"
+            echo "$output_dir not writable"
             exit 1
         fi
-
-        #Assume parser prints to file line by line
-        IFS=$(echo -en "\n\b")
-        STATUS_CHECK=0
-        exec 3<$FILE
-        while read -u 3 -r line
+        if [ -e "/var/log/gpsFix.log" ]; then
+            FILE="/var/log/gpsFix.log"
+            
+        else
+            FILE_CHECK=1
+            echo "/var/log/gpsFix.log does not exist"
+            exit 1
+        fi
+        D_SPACE=(df -k "$output_dir" | tail -1 | tr -s ' ' | cut -d' ' -f4)
+        if [ "$D_SPACE" -ge "6000000" ]; then
+            echo "Sufficient available disk space"
+        else
+            DISK_SPACE=1
+            echo "Not enough disk space"
+            exit 1
+        fi
+        
+        # IFS=$(echo -en "\n\b")
+        # exec 3<$FILE
+        # while read -u 3 -r line
+        while true # want to just keep reaccessing the file and look at line
         do
-            #read in line by line until we get a "YES" hit and triggers status change
-            #Change STATUS_CHECK to 1 when goes through all status checks and passes
-            if [ "$line" -eq "YES" ] then
-                STATUS_CHECK=1
+            # IFS=$(echo -en "\n\b")
+            # exec 3<$FILE
+            # read -u 3 -r line
+            line=$(head -n 1 "$FILE")
+            if [ "$line" -eq "TRUE" ]; then
                 echo "GPS lock found!"
                 break
             else
+                GPS_LOCK=1
                 echo "Finding..."
             fi
-
         done
-        ;;
-
-    start)
-        # start
-        if [ "$STATUS_CHECK" -eq "0" ] then
-            echo "GPS Lock not found! Run STATUS to check"
-            exit 1
-        elif [ "$FILE_CHECK" -eq "0" ] then
-            echo "GPS file does not exist! Run STATUS to check"
-            exit 1
-        elif [ "#OUTPUT_CHECK" -eq "0" ] then
-            echo "Confirm OUTPUT_CHECK"
-        elif [ ! -f /var/lock/rctstart ] then
-            $INSTALL_DIR/bin/rctrun &
-            echo "Service started!"
-            touch /var/lock/rctstart
+        if ["$OUTPUT_CHECK" -ne "1" ] && [ "#FILE_CHECK" -ne "1" ] && [ "$GPS_LOCK" -ne "1"] && [ "$SDR_CHECK" -ne "1"]; then
+            $OVERALL_STATUS=1
+            echo "All checks complete"
+            break
         fi
-        exit
-        ;;
-    stop)
-        killall rct -r &
-        echo "Stopping Services!"
-        rm -f /var/lock/rctstart
-        exit
-        ;;
+    done
 
-    restart|reload|condrestart)
-        killall rct -r &
-        echo "Service started!"
-        $INSTALL_DIR/bin/rctrun &
-        exit
-        ;;
-esac
+    if [ "$OVERALL_STATUS" -eq "0" ]; then
+        functions()
+    fi
+}
 
+functions() {
+    read -p "Do you with to start, stop, or restart drone?" input
+    case "$input" in
+        start)
+            # start
+            if [ "$GPS_LOCK" -eq "0" ]; then
+                echo "GPS Lock not found!"
+                exit 1
+            elif [ "$FILE_CHECK" -eq "0" ]; then
+                echo "GPS file does not exist!"
+                exit 1
+            elif [ "$OUTPUT_CHECK" -eq "0" ]; then
+                echo "Confirm writable output"
+            elif [ ! -f /var/lock/rctstart ]; then
+                $INSTALL_DIR/bin/rctrun &
+                echo "Service started!"
+                touch /var/lock/rctstart
+            fi
+            exit
+            ;;
+        stop)
+            killall rct -r &
+            echo "Stopping Services!"
+            rm -f /var/lock/rctstart
+            status()
+            exit
+            ;;
+        restart|reload|condrestart)
+            killall rct -r &
+            echo "Service restarted!"
+            $INSTALL_DIR/bin/rctrun &
+            status()
+            exit
+            ;;
+    esac
+}

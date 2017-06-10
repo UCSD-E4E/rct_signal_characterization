73afe113e27d70ad7d2b6f5b4da0b57deafec625
svineyar <svineyar@ucsd.edu>
2017-06-10 03:56:27 -0700

    Web interface now has full backend functionality for status checks and user input for start/stop.

--- Web_Interface/index.php
+++ Web_Interface/index.php
@@ -17,40 +17,79 @@
 		// Read the status from a text file that has 6 digit number. Each
 		// number presentst the LOW/HIGH value of the status check with 
 		// the 6th digit representing the start/stop data logging switch
-		exec("cut -c".$i." /home/root/status.txt", $status[$i-1] );
-
-		// TO DELETE: 
-		//
-		// set the pin's mode to output and read them
-		// system("gpio mode ".$i." out");
-		// exec ("gpio read ".$i, $val_array[$i], $return );
-		//
+		exec("cut -c".$i." /usr/local/apache2/htdocs/status.txt", $status[$i-1] );
 	}
+
+	echo "<div style='display: block;float: left;margin-right: 5px;'>";
+
 	//Status checks (x5) 
 	$i =0;	// IS THIS NECESSARY?
 	for ($i = 0; $i < 5; $i++) {
+
+		echo "<div class='image' style='display: block;float: left;margin-right: 5px;'>";
+
 		//if status IS NOT checked
 		if ($status[$i][0] == 0 ) {
-			echo ("	<img id='check_".$i."' src='images/x.jpg' />");
+			echo ("	<img id='check_".$i."' src='images/x.jpg' 
+				width='175px' height='175px'/>");
 			echo "\r\n";
 		}
 		//if status IS checked 
 		if ($status[$i][0] == 1 ) {
-			echo ("	<img id='check_".$i."' src='images/check.jpg' />");
+			echo ("	<img id='check_".$i."' src='images/check.jpg' width='175px' height='175px' />");
 			echo "\r\n";
 		}	 
+
+		// add caption based on status #
+		if ($i == 0) {
+			echo "<div style='display:table-caption;caption-side:bottom;'>Sane Output Directory</div>";
+			echo "\r\n";
+		}
+		else if ($i == 1) {
+			echo "<div style='display:table-caption;caption-side:bottom;'>Output Directory Writable</div>";
+			echo "\r\n";
+		}
+		else if ($i == 2) {
+			echo "<div style='display:table-caption;caption-side:bottom;'>SDR Verified</div>";
+			echo "\r\n";
+		}
+		else if ($i == 3) {
+			echo "<div style='display:table-caption;caption-side:bottom;'>Available Disk Space</div>";
+			echo "\r\n";
+		}
+		else if ($i == 4) {
+			echo "<div style='display:table-caption;caption-side:bottom;'>3D GPS Fix</div>";
+			echo "\r\n";
+		}
+
+		echo "</div>";
+
 	}
 
-	//Drone start/stop toggle switch icon 
+	echo "</div>";
+
+	//start/stop toggle switch icon 
+
+	echo "<div class='image' style='display:table;'>";
+
 	if ($status[5][0] == 0) {
-		echo ("	<img id='toggle_".$i."' src='images/toggleOff.jpg' />");
+		echo ("	<img id='toggle' src='images/toggleOff.jpg' onclick='toggle_switch ();' 
+			width='350px' height='190px' />");
+		echo "\r\n";
 	}
 	elseif ($status[5][0] == 1) {
-		echo ("	<img id='toggle_".$i."' src='images/toggleOn.jpg' />");
+		echo ("	<img id='toggle' src='images/toggleOn.jpg' onclick='toggle_switch ();' 
+			width='350px' height='190px'/>");
+		echo "\r\n";
 	}
+	echo "<div style='display:table-caption;caption-side:bottom;'>Toggle Data Logging</div>";
+	echo "\r\n";
+	echo "</div>";
+
 	?>
 	 
 	<!-- javascript -->
-	<!--script src="update.js"></script-->
+	<script src="update.js"> </script>
+	<script type="text/javascript">status_loop ();</script>
     </body>
 </html>
---
+++ Web_Interface/update.js
@@ -0,0 +1,119 @@
+﻿// Update web interface page depending on status check changes and toggle switch.
+// User can now interact with toggle switch on web interface. 
+
+//Status checks and toggle 
+var sane_output     = document.getElementById("check_0");
+var output_writable = document.getElementById("check_1");
+var verify_SDR      = document.getElementById("check_2");
+var disk_space	    = document.getElementById("check_3");
+var gps_lock        = document.getElementById("check_4");
+var toggle	        = document.getElementById("toggle");
+
+//Create an array of status checks for easy access later
+var Status_checks = [ sane_output, output_writable, verify_SDR, disk_space, gps_lock ];
+
+//This function is asking for gpio.php, receiving datas and updating the index.php pictures
+function toggle_switch () {
+var data = 0;
+//send the pic number to gpio.php for changes
+//this is the http request
+	var request = new XMLHttpRequest();
+	request.open( "GET" , "toggle.php", true);
+	request.send(null);
+	//receiving informations
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+			//update the index pic
+			if ( data == "0" ) {
+				toggle.src = "images/toggleOff.jpg";
+			}
+			else if ( data == "1" ) {
+				toggle.src = "images/toggleOn.jpg";
+			}
+			else if ( !(data.localeCompare("fail"))) {
+				alert ("Data returned 'fail'" );
+				return ("fail");			
+			}
+			else {
+				alert ("Default fail: No other data if condition was met!" );
+				return ("fail"); 
+			}
+		}
+		//test if fail
+		else if (request.readyState == 4 && request.status == 500) {
+			alert ("server error");
+			return ("fail");
+		}
+		//else 
+		else if (request.readyState == 4 && request.status != 200 && request.status != 500 ) { 
+			alert ("Default fail: Success 200 or General Fail 500 Not Found Though");
+			return ("fail"); }
+	}	
+	
+return 0;
+}
+
+// Calls status_check every 1 seconds 
+function status_loop () {
+	setInterval(status_check, 500);
+}
+
+//Called by status_loop.
+//This function is asking for status.php, receiving status value, and updating the status icons
+function status_check () {
+	var data = 0; 
+
+	//this is the http request
+	var request = new XMLHttpRequest();
+	request.open( "GET" , "status.php", true);
+	request.send(null);
+	//receiving informations
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+
+			console.log(data);
+
+			// Loop through each status value and change pics accordingly 
+			for (i = 0; i <6; i++) {
+
+				// update toggle switch icon if user toggled physical switch 
+				if (i == 5) {
+					if ( data.charAt(i) == "0" && toggle.src != "images/toggleOff.jpg") {
+						toggle.src = "images/toggleOff.jpg";
+					}
+					else if ( data.charAt(i) == "1" && toggle.src != "images/toggleOn.jpg" ) {
+						toggle.src = "images/toggleOn.jpg"; 
+					}
+				}
+
+				// update status check icons 
+				else if ( data.charAt(i) == "0" && Status_checks[i].src != "images/x.jpg") {
+					Status_checks[i].src = "images/x.jpg";
+				}
+				else if ( data.charAt(i) == "1" && Status_checks[i].src != "images/check.jpg" ) {
+					Status_checks[i].src = "images/check.jpg";
+				}
+				else {
+					alert ("Default fail: No other data if condition was met!" );
+					return ("fail"); 
+				}	
+			}
+		}
+
+		//test if fail
+		else if (request.readyState == 4 && request.status == 500) {
+			alert ("server error");
+			return ("fail");
+		}
+		//else 
+		else if (request.readyState == 4 && request.status != 200 && request.status != 500 ) { 
+			alert ("Default fail: Success 200 or General Fail 500 Not Found Though");
+			return ("fail"); }
+
+		}		
+	//}	
+	
+return 0;
+}
